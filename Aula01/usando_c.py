51d161b6acf1507d23bde6407ef63cdf214eaaa2
Mei <149503745+Maysa502@users.noreply.github.com>
2024-01-13 22:42:06 -0300

    addcomentariosIII

--- Aula01/usando_c.py
+++ Aula01/usando_c.py
@@ -1,5 +1,6 @@
 # Usar o comando d importação para a biblioteca criada 
 import ctypes
+# O uso do módulo ctypes em Python é uma forma de criar uma ponte entre Python e C
 
 lib = ctypes.CDLL("./mensagem.so")
 lib.mensagem()
