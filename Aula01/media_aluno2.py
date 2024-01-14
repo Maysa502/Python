00ea6a9185d2b49e152d89e7e1ecde24d223e8ae
Mei <149503745+Maysa502@users.noreply.github.com>
2024-01-13 21:17:51 -0300

    addcomentario

--- Aula01/media_aluno2.py
+++ Aula01/media_aluno2.py
@@ -19,6 +19,9 @@ media = (primb + segb + tercb + quarb) / 4
 media = float(media) 
 if media >= 6:
     print("Aluno aprovado")
+    
+# A palavra-chave "elif" é utilizada para adicionar uma condição adicional a ser verificada se a condição do "if" não for satisfeita
+        # Se colocar mais um 'if', em alguns casos, o código irá executar o ">= 6" e tambem o "else" ja que ambos teriam a media maior que 4 (>=4)  
 elif media <= 4:
     print ("Aluno reprovado")
 else:
