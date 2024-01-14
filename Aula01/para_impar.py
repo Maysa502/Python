93052cc8452e1d9ce40bc23b39b04ef0e05b447d
Mei <149503745+Maysa502@users.noreply.github.com>
2024-01-13 22:33:29 -0300

    addcomentariosII

--- Aula01/para_impar.py
+++ Aula01/para_impar.py
@@ -1,7 +1,17 @@
 numero = input("Digite um numero: ")
 # o comando input sempre retorna um valor em fomato texto, não importa se foi digitado um valor numerico, ele sempre retorna um texto
-# sendo assim, foi necessario converter a variavel 'numero para um valor inteiro. Neste caso, coloquei o int(inteiro na frente da vari)  
-if int(numero) %2 == 0:
+# sendo assim, foi necessario converter a variavel 'numero' para um valor inteiro. Neste caso, coloquei o int(inteiro na frente da vari)
+
+    # se um "inteiro" "numero" for "dividido por 2" e o 'resto' for "semelhante" a "0":
+if int(numero) %2 == 0: 
     print("O numero digitado é par")
 else:
     print("O numero digitado é impar")
+
+
+# outa forma de realizar este algoritimo é: Add a equação na variavel
+    # numero = int(numero) %2 ==0
+        # if (numero):
+            # print("Este é um algarismo par")
+        # else:
+            # print("Este algorismo é impar")
