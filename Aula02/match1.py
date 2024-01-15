0c4da65ee7e1c78746266007b153b9d2ad261e68
Mei <149503745+Maysa502@users.noreply.github.com>
2024-01-15 20:36:06 -0300

    addcomentariosIX

--- Aula02/match1.py
+++ Aula02/match1.py
@@ -1,3 +1,8 @@
+''' Aqui é um exemplo de match case: 
+A sintaxe match case é útil quando você tem várias opções ou casos diferentes a considerar, como em uma situação de escolha, 
+decisão ou correspondência de padrões, em um visual mais claro, imagine que um usuario tem varias opções de cadastro, essa sintaxe
+é bem util de otimizar os codigos''' 
+
 dia = input("Digite um numero entre 1 e 7, e lhe diremos qual o dia da semana: ")
 
 match dia:
