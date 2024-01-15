e25c5c8cde61a8d688d3a4263abbc5c748aff52f
Mei <149503745+Maysa502@users.noreply.github.com>
2024-01-15 20:51:23 -0300

    addcomentariosX

--- Aula02/while1.py
+++ Aula02/while1.py
@@ -5,3 +5,19 @@ while(n <= 10):
     print(n)
     n+=1
     #n ta começando pelo n = 1 ja que foi indicado ali 
+
+
+
+
+
+''' FOR X WHILE  
+For:
+Use quando você sabe exatamente quantas vezes quer fazer algo.
+É como contar os dedos: você conta de 1 a 10, por exemplo.
+
+While:
+Use quando quer fazer algo enquanto uma condição for verdadeira.
+É como continuar pulando enquanto a música está tocando.
+
+Então, for é como uma contagem fixa, e while é como repetir algo até que algo aconteça ou deixe de ser verdadeiro. 
+Ambos são úteis, mas escolha o que faz mais sentido para o que você quer fazer no seu código.'''
