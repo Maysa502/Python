1f10d5edba9b48ca06aee05631e8a4c62bfb24d5
Senac Tatuape <senac@localhost.localdomain>
2024-01-18 11:52:38 -0300

    Funções

---
+++ revisão/funcao.py
@@ -0,0 +1,12 @@
+#  Este algotmo possui uma função que diz quantas lestras tem um texto
+
+#  Para criar uma função é necessario utilizar o comando def
+
+def qtletras(texto):
+    '''
+    A função que tem lestras recebe um texto e exibe a quantidade de letras presentes no texto
+    '''
+    print(len(texto))
+
+frase = "korra é uma linda"
+qtletras(frase)
---
+++ revisão/funcao3.py
@@ -0,0 +1,11 @@
+# importando um arquivo
+# alias é tipo um apelido só colocar um 'as'
+# import funcao2 as fn - td o arquivo
+
+import funcao2 from soma as sm
+
+n = [1,2,3,4,5]
+print(sm(n))
+
+# n é o que o soma vai calcular, no caso o que esta dentro do 'n'
+# print(fn.maior(n))
