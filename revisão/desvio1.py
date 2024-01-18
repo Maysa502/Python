245b8eae4c4a88b272c68a408c7e412597c48879
Senac Tatuape <senac@localhost.localdomain>
2024-01-18 09:53:54 -0300

    Trabalhando com desvio de fluxo if

---
+++ revisão/desvio1.py
@@ -0,0 +1,16 @@
+# Este algoritmos pbtem o nome do aluno e a media do Aluno
+
+nome = input("Digite o nome do aluno: ")
+sobrenome = input("Digite o sobrenome do aluno ")
+media = input("Digite a nota media do aluno: ")
+media = float(media)
+
+if(media >= 6):
+    print(nome + " esta aprovado")
+
+else:
+    print("Reprovado")
+
+# nota1 = input("Digite a nota do primeiro semestre: ", nome)
+# nota2 = input("Digite a nota do segundo bimestre do ",nome)
+# nota3 = 
---
+++ revisão/desvio2.py
@@ -0,0 +1,17 @@
+# Este algotmos exibe uma mensagem para os motoristas que estão com o seus carros em dias de rodizio
+
+final_placa = input("Digite o numero final da placa do teu carro: ")
+# aqui usamos os sinais de comparação, não pra atribuir a valor mas sim em comparar
+if(final_placa == "1" or final_placa == "2"):
+    print("Seu carro se enquadra na zona de rodizio - Segunda-Feira")
+elif(final_placa == "3" or final_placa == "4"):
+    print("Seu final de placa esta no dia de rodizio - Terça-Feira")
+elif(final_placa == "5" or final_placa == "6"):
+    print("Seu final de placa esta no dia de rodizio - Quarta-Feira")
+elif(final_placa == "7" or final_placa == "8"):
+    print("Seu final de placa esta no dia de rodizio - Quinta-Feira")
+elif(final_placa == "9" or final_placa == "0"):
+    print("Seu final de placa esta no dia de rodizio - Sexta-Feira")
+else:
+    print("Final de placa não encontrado :(")
+
