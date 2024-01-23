f6f55aee5e5923613cfad9ee3a1faa39fd7851cc
Senac Tatuape <senac@localhost.localdomain>
2024-01-23 09:37:46 -0300

    Adição do metodo sacar da conta corrente

--- POO/dados/class_conta_corrente.py
+++ POO/dados/class_conta_corrente.py
@@ -12,3 +12,17 @@ class ContaCorrente(ContaBancaria):
         self._saldo=saldo
         self.__limite=limite
         print("A conta "+self._nconta+" do Sr(a). "+self._titular+" foi aberta")    
+
+    def sacar(self, valor):
+        
+        if(valor > self._saldo+self.__limite):
+            print("Saldo insuficiente")
+
+        elif(valor <= self._saldo):
+            self._saldo -= valor
+            print("Você realizou um saldo de "+str(valor) +" o seu saldo atual é "+str(self._saldo))
+        else:
+            sobra = valor - self._saldo
+            self.__limite -= sobra
+            self.saldo = 0 
+            print("O valor retirado é "+str(valor)+" \nO saldo atual é: "+str(self._saldo)+ " \nO seu limite atual é "+str(self.__limite))
