2b5bb10d77ed4242b50292da6a1af3298e36e0e7
Senac Tatuape <senac@localhost.localdomain>
2024-01-23 08:30:32 -0300

    Trabalho com herança em python

---
+++ POO/dados/class_conta_bancaria.py
@@ -0,0 +1,8 @@
+class ContaBancaria:
+    def __init__(self):
+        self._nbanco=0
+        self._nagencia=0
+        self.nconta=""
+        self.titular=""
+        self.saldo=0.0
+        
---
+++ POO/dados/obj_conta_corrente.py
@@ -0,0 +1,4 @@
+from class_conta_corrente import ContaCorrente as cc
+
+conta = cc()
+conta.abrirConta(35,12,"234465-10","Márcia",3500,1000)
